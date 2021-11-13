499f1d0cafe9d22a3f537cdd05e76cd614a5a12c
qigong.hu@tuya.com <qigong.hu@tuya.com>
2021-11-13 13:37:22 +0800

    add server poll

--- linux_socket_example/server.c
+++ linux_socket_example/server.c
@@ -156,6 +156,123 @@ int socketSever_Init_select(void)
 
     return 0;
 }
+
+int socketSever_Init_poll(void)
+{
+    struct sockaddr_in serv_addr;
+    int stat,tr;
+
+    printf("socketSeverInit\n");
+    // New socket
+    listen_socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_socketFd < 0) 
+    {
+       printf("ERROR opening socket\n");
+       return -1;
+    }
+ 
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+	  serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(778);
+
+    stat = bind(listen_socketFd, (struct sockaddr *) &serv_addr,sizeof(serv_addr));
+    if ( stat < 0) 
+    {
+      printf("ERROR on binding: %s\n", strerror( errno ) );
+      exit(1);
+    }
+    //will have 5 pending open client requests
+    listen(listen_socketFd,5); 
+    // Set the fd to none blocking
+    fcntl(listen_socketFd, F_SETFL, O_NONBLOCK);
+    // Set the socket option SO_REUSEADDR to reduce the chance of a 
+    // "Address Already in Use" error on the bind
+    setsockopt(listen_socketFd,SOL_SOCKET,SO_REUSEADDR,&tr,sizeof(int));
+
+  	printf("init nonblock listen_socketFd=%d\n",listen_socketFd);
+    for(int i = 0; i < MAX_ACCEPT; i ++){
+        client_fd[i] = -1;
+        memset(&client_addr[i], 0x0, sizeof(struct sockaddr_in));
+    }
+    for(;;)
+    {
+        //poll init
+        struct pollfd *pollFds = (struct pollfd *)malloc(  ((MAX_ACCEPT + 1) * sizeof( struct pollfd )) );
+        if(pollFds){
+            //把监听fd放到数组，后面好遍历
+            client_fd[0] = listen_socketFd;
+            //add fd to poll list
+            for(int i = 0; i < MAX_ACCEPT+1; i ++){
+                //printf("client_fd[%d]=%d\r\n",i,client_fd[i]);
+                if(client_fd[i]!=-1){
+                    pollFds[i].fd = client_fd[i];
+                    pollFds[i].events = POLLIN |POLLHUP|POLLNVAL|POLLERR|POLLRDHUP;
+                }
+            }
+            //poll阻塞监听是ms级别的，10s，若超时poll返回0，有事件发生poll返回大于0
+            int rc = poll(pollFds, (MAX_ACCEPT+1), 1000);
+            /*监听超时返回*/
+            if (rc == 0) {
+                /*can do some things*/
+            }else{
+                printf("poll rc is: %d\n", rc);
+                /*rc > 0 有数据，这里select/poll都一样，需要自己在监听list中找到有事件的fd*/
+                for(int Idx=0; Idx < (MAX_ACCEPT+1); Idx++)
+                {
+                    if(pollFds[Idx].revents) 
+                    {
+                        /**/
+                        if(pollFds[Idx].fd == listen_socketFd){
+                            printf("accept new client\r\n");
+                            //接受一个到server_socket代表的socket的一个连接
+                            //如果没有连接请求,就等待到有连接请求--这是accept函数的特性
+                            // accept函数返回一个新的socket,这个socket(new_server_socket)用于同连接到的客户的通信
+                            // new_server_socket代表了服务器和客户端之间的一个通信通道
+                            // accept函数把连接到的客户端信息填写到客户端的socket地址结构client_addr中
+                            socklen_t length = sizeof (client_addr);
+
+                            int new_server_socket = accept(listen_socketFd, (struct sockaddr *) &client_addr[Idx], &length);
+                            if (new_server_socket != 0){
+                                printf("new_server_socket=%d\r\n",new_server_socket);
+                                fcntl(new_server_socket, F_SETFL, O_NONBLOCK);
+                                for(int n=0;n<MAX_ACCEPT+1;n++){
+                                    if(client_fd[n] == -1){
+                                        client_fd[n] = new_server_socket;
+                                                break;
+                                    }
+                                }
+                            }
+                        }else{
+                            printf("receive data\r\n");
+                            //receive data
+                            bzero(buffer, BUFFER_SIZE);
+                            int readlength = (int)read(client_fd[Idx], buffer,1024);
+                            if (readlength < 0)
+                            {
+                                printf("Server Recieve Data Failed!\n");
+                                //关闭listen socket
+                                for(int n=0;n<MAX_ACCEPT+1;n++){
+                                    if(client_fd[n]== client_fd[Idx]){
+                                        client_fd[n] = -1;
+                                        break;
+                                    }
+                                }
+                                close(client_fd[Idx]);
+                            }else{
+                                handleReceiveData((unsigned char*)buffer,readlength);
+                            }
+                        }
+                    }
+                }
+            }
+            free(pollFds);
+        }
+    }//for(;;)
+    //关闭与客户端的连接
+    close(listen_socketFd);
+    return 0;
+}
 void* main(void*arg)
 {
     socketSever_Init_select();
