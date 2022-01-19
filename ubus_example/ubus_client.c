e216733729c6314e82d149cc57304d8d0317ea55
huruitao2013@126.com <qigong.hu@tuya.com>
2022-01-19 21:23:06 +0800

    json-c

--- ubus_example/ubus_client.c
+++ ubus_example/ubus_client.c
@@ -30,6 +30,74 @@ struct blob_buf b;
 struct ubus_context *ubus_ctx;
 struct ubus_event_handler listener;
 
+__attribute__((unused))void UbusInitiator::scanreq_prog_cb1(struct ubus_request *req, int type, struct blob_attr *msg)
+{
+	char *str;
+    int errCode;
+    struct json_object* ObjMember=NULL;
+	if (!msg)
+		return;
+	/* 
+	在这里处理返回的消息。
+	本例子只是将返回的消息打印出来。
+	*/
+	str = blobmsg_format_json_indent(msg, true, 0);
+	printf("%s\n", str);
+    struct json_object* obj = json_tokener_parse(str);
+    if(obj){
+        struct json_object* ObjError = json_object_object_get(obj,"Error");
+        if(ObjError){
+            errCode=json_object_get_int(ObjError);
+            if(errCode==0){
+                if(NULL!=(ObjMember=json_object_object_get(obj,"PanId"))){
+                    std::string panidStr=json_object_get_string(ObjMember);
+                    std::cout<<"panidStr = "<<panidStr<<std::endl;
+                    OtbrManager::GetInstance()->OtbrSetPanId(stoi(panidStr,nullptr,16));
+                    std::cout<<"PanId ="<<OtbrManager::GetInstance()->OtbrGetPanId()<<std::endl;
+                    OtbrManager::GetInstance()->mOtbrDelegate->OtbrNetworkNameQuery();
+                }else if(NULL!=(ObjMember=json_object_object_get(obj,"NetworkName"))){
+                    OtbrManager::GetInstance()->OtbrSetNetworkName(json_object_get_string(ObjMember));
+                    std::cout<<"NetworkName ="<<OtbrManager::GetInstance()->OtbrGetNetworkName()<<std::endl;
+                    OtbrManager::GetInstance()->mOtbrDelegate->OtbrRloc16Query();
+                }else if(NULL!=(ObjMember=json_object_object_get(obj,"Channel"))){
+                    OtbrManager::GetInstance()->OtbrSetChannel(json_object_get_int(ObjMember));
+                    std::cout<<"Channel ="<<OtbrManager::GetInstance()->OtbrGetChannel()<<std::endl;
+                    OtbrManager::GetInstance()->mOtbrDelegate->OtbrPanIdQuery();
+                }else if(NULL!=(ObjMember=json_object_object_get(obj,"rloc16"))){
+                    size_t idx=2;
+                    std::string rloc16Str=json_object_get_string(ObjMember);
+                    OtbrManager::GetInstance()->OtbrSetRloc16(stoi(rloc16Str,&idx,16));
+                    std::cout<<"rloc16 ="<<OtbrManager::GetInstance()->GetInstance()->OtbrGetRloc16()<<std::endl;
+                    OtbrManager::GetInstance()->mOtbrDelegate->OtbrExtendPanIdQuery();
+                }else if(NULL!=(ObjMember=json_object_object_get(obj,"ExtPanId"))){
+                    OtbrManager::GetInstance()->OtbrSetExtendPanId(json_object_get_string(ObjMember));
+                    std::cout<<"ExtPanId ="<<OtbrManager::GetInstance()->OtbrGetExtendPanId()<<std::endl;
+                    OtbrManager::GetInstance()->mOtbrDelegate->OtbrNetworkKeyQuery();
+                }else if(NULL!=(ObjMember=json_object_object_get(obj,"Networkkey"))){
+                    OtbrManager::GetInstance()->OtbrSetNetworkKey(json_object_get_string(ObjMember));
+                    std::cout<<"Networkkey ="<<OtbrManager::GetInstance()->OtbrGetNetworkKey()<<std::endl;
+                    OtbrManager::GetInstance()->mOtbrDelegate->OtbrPskcQuery();
+                }else if(NULL!=(ObjMember=json_object_object_get(obj,"pskc"))){
+                    OtbrManager::GetInstance()->OtbrSetPskc(json_object_get_string(ObjMember));
+                    std::cout<<"pskc ="<<OtbrManager::GetInstance()->OtbrGetPskc()<<std::endl;
+                }else{
+                    std::cout<<": "<<__LINE__<< ",errCode = "<<errCode<<std::endl;
+                }
+            }else{
+                std::cout<<": "<<__LINE__<< ",errCode = "<<errCode<<std::endl;
+            }
+        }else{
+            std::cout<<":%d "<<__LINE__<<std::endl;
+        }
+        json_object_put(obj);
+    }else{
+        std::cout<<":%d "<<__LINE__<<std::endl;
+    }
+
+	free(str);
+    return ;
+}
+
 static void scanreq_prog_cb(struct ubus_request *req, int type, struct blob_attr *msg)
 {
 	char *str;
